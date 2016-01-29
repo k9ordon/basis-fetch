5f0d4bb79d40b3ec2e36b6cb8e513a21b39f1a8e
kschreiber <klemens.schreiber@karriere.at>
2016-01-29 16:18:22 +0100

    added Bodystates data

--- index.js
+++ index.js
@@ -45,12 +45,20 @@ var getMetrics = function(access_token, date, dataType, callback) {
                     '&heartrate=true' +
                     '&steps=true' +
                     '&calories=true' +
-                    '&gsr=true' + 
-                    '&skin_temp=true' + 
+                    '&gsr=true' +
+                    '&skin_temp=true' +
                     '&air_temp=true';
 
 
-    getData(metricURL, access_token, date, dataType, formatMetric, callback);            
+    getData(metricURL, access_token, date, dataType, formatMetric, callback);
+};
+
+var getBodystates = function(access_token, date, dataType, callback) {
+    var requestDate = dateFormat(date, 'yyyy-mm-dd');
+    var metricURL = 'https://app.mybasis.com/api/v1/bodystatesday/me?day=' + requestDate +
+                    '&padding=0';
+
+    getData(metricURL, access_token, date, dataType, formatBodystates, callback);
 };
 
 var getData = function(url, access_token, date, dataType, formatter, callback) {
@@ -67,10 +75,10 @@ var getData = function(url, access_token, date, dataType, formatter, callback) {
             json: true
         }, function (err, respone, data) {
             if (err) {
-                console.log(err);   
+                console.log(err);
                 callback(err, null);
             }
-      
+
             var formattedData = formatter(data);
 
             callback(null, formattedData);
@@ -82,7 +90,7 @@ var getData = function(url, access_token, date, dataType, formatter, callback) {
 var formatMetric = function (data) {
     var metrics = data.metrics;
     var metricsCount = metrics.heartrate.values.length;
-    
+
     var metricTime = new Date();
     var startTime = moment.unix(data.starttime);
     var metricArray = [];
@@ -95,7 +103,7 @@ var formatMetric = function (data) {
         "skinTemp"  : 0,
         "airTemp"   : 0
     };
-    
+
     for (var i = 0; i < metricsCount; i++) {
         metricTime = startTime.add(1, 'minutes');
         metricItem.date = metricTime.unix();
@@ -105,7 +113,7 @@ var formatMetric = function (data) {
         metricItem.gsrs = metrics.gsr.values[i];
         metricItem.skinTemp = metrics.skin_temp.values[i];
         metricItem.airTemp = metrics.air_temp.values[i];
-        
+
         metricArray.push(metricItem);
         metricItem = {
             "date"      : 0,
@@ -119,6 +127,24 @@ var formatMetric = function (data) {
     }
 
     return metricArray;
+};
+
+var formatBodystates = function (data) {
+    var bodystates = data.bodystates;
+    var bodystatesCount = bodystates.length;
+	var bodystatesArray = [];
+
+	console.log('bodystatesCount', bodystatesCount);
+
+    for (var i = 0; i < bodystatesCount; i++) {
+        bodystatesArray.push({
+			start: new Date(bodystates[i][0] * 1000),
+			end: new Date(bodystates[i][1] * 1000),
+			activity: bodystates[i][2]
+		});
+    }
+
+    return bodystatesArray;
 };
 
 var getLatest = function (data, callback) {
@@ -141,5 +167,6 @@ var getLatest = function (data, callback) {
 module.exports = {
     "requestUser": requestUser,
     "getMetrics": getMetrics,
+	"getBodystates": getBodystates,
     "getLatest": getLatest
 };
